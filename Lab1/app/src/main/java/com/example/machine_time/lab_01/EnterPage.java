075a68c24043a2125d85c9edc023f5664a03167b
Iskhakoff <dima.iskhakov@gmail.com>
2018-03-04 16:51:30 +0300

    fixed bags

--- Lab1/app/src/main/java/com/example/machine_time/lab_01/EnterPage.java
+++ Lab1/app/src/main/java/com/example/machine_time/lab_01/EnterPage.java
@@ -4,10 +4,12 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.QuickContactBadge;
+import android.widget.Toast;
 
 public class EnterPage extends AppCompatActivity implements View.OnClickListener {
 
@@ -28,8 +30,9 @@ public class EnterPage extends AppCompatActivity implements View.OnClickListener
 
         btnEnter.setOnClickListener(this);
 
-        passPref = getPreferences(0);
+        passPref = getSharedPreferences("pass",0);
         String pass = passPref.getString("savedPassword", "");
+        Log.d("PASS", pass);
         password.setText(pass);
         if(password.getText().toString().equalsIgnoreCase(PW)){
             Intent intent = new Intent(this, QuestionPage.class);
@@ -38,7 +41,7 @@ public class EnterPage extends AppCompatActivity implements View.OnClickListener
     }
 
     void savePw(){
-        passPref = getPreferences(0);
+        passPref = getSharedPreferences("pass", 0);
         SharedPreferences.Editor editor = passPref.edit();
         editor.putString("savedPassword", password.getText().toString());
         editor.commit();
@@ -49,11 +52,17 @@ public class EnterPage extends AppCompatActivity implements View.OnClickListener
         switch (v.getId()){
             case R.id.btnEnter:
                 if(password.getText().toString() != "" && password.getText().toString().equalsIgnoreCase(PW)){
+
+
                     savePw();
 
                     Intent intent = new Intent(this, QuestionPage.class);
                     startActivity(intent);
+                }else{
+                    Toast.makeText(this, "Введен неправильный пароль", Toast.LENGTH_LONG).show();
                 }
+                break;
+
         }
     }
 }
--- Lab1/app/src/main/java/com/example/machine_time/lab_01/QuestionPage.java
+++ Lab1/app/src/main/java/com/example/machine_time/lab_01/QuestionPage.java
@@ -1,8 +1,13 @@
 package com.example.machine_time.lab_01;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RadioButton;
@@ -10,15 +15,17 @@ import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.Map;
+
 public class QuestionPage extends AppCompatActivity implements View.OnClickListener{
 
-    String[] questions = {"Сколько вершин в треугольнике?", "Со скольки лет продают сигареты?",
+    String[] questions = {"Сколько вершин в треугольнике?", "Начальный уровень компетенции разработчика?",
             "Кто открыл Америку?", "Примерное население людей в мире?", "Сколько пальцев на РУКАХ у человека?"};
 
-    String[] answer1 = {"1", "16", "Колумб", "10 млрд", "5"};
-    String[] answer2 = {"5", "18", "Навальный", "7 млрд", "20"};
-    String[] answer3 = {"3", "21", "Обама", "14 млрд", "10"};
-    String[] correctAnswer = {"3", "18", "Колумб", "7 млрд", "10"};
+    String[] answer1 = {"1", "Junior", "Христофор Колумб", "10 млрд", "5"};
+    String[] answer2 = {"5", "Middle/Regular", "Алексей Навальный", "7 млрд", "20"};
+    String[] answer3 = {"3", "Senior", "Барак Обама", "14 млрд", "10"};
+    String[] correctAnswer = {"3", "Junior", "Колумб", "7 млрд", "10"};
 
     TextView questionTv;
     RadioButton ansTv1, ansTv2, ansTv3;
@@ -26,10 +33,12 @@ public class QuestionPage extends AppCompatActivity implements View.OnClickListe
     RadioGroup answers;
 
 
+
+
     Question[] allQuestions = {new Question(), new Question(), new Question(), new Question(), new Question()};
 
-    int count = 0;
-    int j = 0;
+    int countCorrect = 0;
+    int countPageQuest = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,52 +61,125 @@ public class QuestionPage extends AppCompatActivity implements View.OnClickListe
             allQuestions[i].setAnswer3(answer3[i]);
         }
 
-        showQuest(j);
+        showQuest(countPageQuest);
+    }
 
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
     }
-    void showQuest(int j){
-        questionTv.setText(allQuestions[j].getQuestion());
-        ansTv1.setText(allQuestions[j].getAnswer1());
-        ansTv2.setText(allQuestions[j].getAnswer2());
-        ansTv3.setText(allQuestions[j].getAnswer3());
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        SharedPreferences cleanPass;
+        switch (item.getItemId()){
+            case R.id.menu_about:
+                Intent intent = new Intent(this, AboutPage.class);
+                startActivity(intent);
+                break;
+            case R.id.menu_exit:
+                cleanPass = getSharedPreferences("pass", 0);
+                cleanPass.edit().remove("savedPassword").apply();
+                Intent forgotPass = new Intent(this, EnterPage.class);
+                startActivity(forgotPass);
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
+
+    void showQuest(int countPageQuest){
+        questionTv.setText(allQuestions[countPageQuest - 1].getQuestion());
+        ansTv1.setText(allQuestions[countPageQuest - 1].getAnswer1());
+        ansTv2.setText(allQuestions[countPageQuest - 1].getAnswer2());
+        ansTv3.setText(allQuestions[countPageQuest - 1].getAnswer3());
+    }
+
+
 
     @Override
     public void onClick(View v) {
         if(v.getId() == R.id.btnReply){
-            switch (answers.getCheckedRadioButtonId()){
-                case R.id.ansTv1:
-                    if(ansTv1.getText().toString().equalsIgnoreCase(correctAnswer[j])){
-                        count++;
-                    }
-                    answers.clearCheck();
-                    j++;
-                    showQuest(j);
-                    Log.d("count", String.valueOf(count));
-                    break;
-                case R.id. ansTv2:
-                    if(ansTv2.getText().toString().equalsIgnoreCase(correctAnswer[j])){
-                        count++;
-                    }
-                    answers.clearCheck();
-                    j++;
-                    showQuest(j);
-                    Log.d("count", String.valueOf(count));
-                    break;
-                case R.id.ansTv3:
-                    if(ansTv3.getText().toString().equalsIgnoreCase(correctAnswer[j])){
-                        count++;
-                    }
-                    answers.clearCheck();
-                    j++;
-                    showQuest(j);
-                    Log.d("count", String.valueOf(count));
-                    break;
-                default:
-                    Toast.makeText(this, "Выберите ответ", Toast.LENGTH_SHORT).show();
-            }
 
+            if(countPageQuest != allQuestions.length){
+                switch (answers.getCheckedRadioButtonId()){
+                    case R.id.ansTv1:
+                        if(ansTv1.getText().toString().equalsIgnoreCase(correctAnswer[countPageQuest - 1])){
+                            countCorrect++;
+                        }
+                        answers.clearCheck();
+                        countPageQuest++;
+                        showQuest(countPageQuest);
+                        Log.d("count", String.valueOf(countCorrect));
+                        Log.d("page", String.valueOf(countPageQuest));
+                        break;
+                    case R.id. ansTv2:
+                        if(ansTv2.getText().toString().equalsIgnoreCase(correctAnswer[countPageQuest - 1])){
+                            countCorrect++;
+                        }
+                        answers.clearCheck();
+                        countPageQuest++;
+                        showQuest(countPageQuest);
+                        Log.d("count", String.valueOf(countCorrect));
+                        Log.d("page", String.valueOf(countPageQuest));
+                        break;
+                    case R.id.ansTv3:
+                        if(ansTv3.getText().toString().equalsIgnoreCase(correctAnswer[countPageQuest - 1])){
+                            countCorrect++;
+                        }
+                        answers.clearCheck();
+                        countPageQuest++;
+                        showQuest(countPageQuest);
+                        Log.d("count", String.valueOf(countCorrect));
+                        Log.d("page", String.valueOf(countPageQuest));
+                        break;
+                    default:
+                        Toast.makeText(this, "Выберите ответ", Toast.LENGTH_SHORT).show();
+
+                }
+            }else{
+                switch (answers.getCheckedRadioButtonId()){
+                    case R.id.ansTv1:
+                        if(ansTv1.getText().toString().equalsIgnoreCase(correctAnswer[countPageQuest - 1])){
+                            countCorrect++;
+                        }
+                        answers.clearCheck();
+                        showResult();
+                        Log.d("count", String.valueOf(countCorrect));
+                        Log.d("page", String.valueOf(countPageQuest));
+                        break;
+                    case R.id. ansTv2:
+                        if(ansTv2.getText().toString().equalsIgnoreCase(correctAnswer[countPageQuest - 1])){
+                            countCorrect++;
+                        }
+                        answers.clearCheck();
+                        showResult();
+                        Log.d("count", String.valueOf(countCorrect));
+                        Log.d("page", String.valueOf(countPageQuest));
+                        break;
+                    case R.id.ansTv3:
+                        if(ansTv3.getText().toString().equalsIgnoreCase(correctAnswer[countPageQuest - 1])){
+                            countCorrect++;
+                        }
+                        answers.clearCheck();
+                        showResult();
+                        Log.d("count", String.valueOf(countCorrect));
+                        Log.d("page", String.valueOf(countPageQuest));
+                        break;
+                    default:
+                        Toast.makeText(this, "Выберите ответ", Toast.LENGTH_SHORT).show();
+
+                }
+            }
         }
+    }
+    void showResult(){
+        Intent intent = new Intent(this, ResultPage.class);
+        intent.putExtra("allAnswers", String.valueOf(allQuestions.length));
+        intent.putExtra("resultTest", String.valueOf(countCorrect));
+        startActivity(intent);
     }
 }
 
