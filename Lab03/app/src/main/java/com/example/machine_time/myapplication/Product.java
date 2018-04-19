67a2f6016ec3246ce79f907373f6f3d8a8c06a0a
Maria <ermilova.m98@mail.ru>
2018-04-19 16:12:22 +0300

    added lab3

---
+++ Lab03/app/src/main/java/com/example/machine_time/myapplication/Product.java
@@ -0,0 +1,24 @@
+package com.example.machine_time.myapplication;
+
+public class Product {
+
+    String title;
+    String country;
+    double rating;
+    String description;
+    int poster;
+
+    public Product(String title, String country, double rating, String description, int poster) {
+        this.title = title;
+        this.country = country;
+        this.rating = rating;
+        this.description = description;
+        this.poster = poster;
+    }
+
+
+
+
+
+
+}
